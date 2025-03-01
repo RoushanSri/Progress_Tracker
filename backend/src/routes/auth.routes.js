9040c5c2c032e1b0ece7321219b68de58c16bff7
RoushanSri <roshansrivastav59796@gmail.com>
2025-03-01 23:02:56 +0530

    feat: add updateUsername route; integrate updateUsername controller in auth routes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getUserProfile, logIn, logOut, signUp, updateAvatar, updatePassword } from '../controllers/auth.controller.js';
+import { getUserProfile, logIn, logOut, signUp, updateAvatar, updatePassword, updateUsername } from '../controllers/auth.controller.js';
 import verifyJwt from '../middlewares/auth.middleware.js';
 const router = express.Router();
 
@@ -9,6 +9,7 @@ router.post('/logout',verifyJwt, logOut);
 router.get('/profile', verifyJwt, getUserProfile);
 router.post('/uploadAvatar',verifyJwt, updateAvatar);
 router.post('/password',verifyJwt, updatePassword);
+router.post('/updateUsername',verifyJwt, updateUsername);
 
 
 export default router;
